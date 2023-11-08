eaeceedcc68ea0ddc9180645d76e4b19d3a4bf31
Alex Nicolaou <alex@stockgamblers.com>
2023-11-08 14:48:49 -0500

    Loading message displayed when we don't know sign in state.

--- admin.dobutsustationery.com/src/routes/+layout.svelte
+++ admin.dobutsustationery.com/src/routes/+layout.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
   import { doc, setDoc } from "firebase/firestore";
-  import { firestore } from "$lib/firebase"
+  import { firestore } from "$lib/firebase";
 
   import { auth, googleAuthProvider } from "$lib/firebase";
   import { Signin, type User } from "@ourway/svelte-firebase-auth";
 
   let me: User = { signedIn: false };
+  let loading = true;
   function user(e: CustomEvent) {
     me = e.detail;
+    loading = false;
     if (me.signedIn) {
       setDoc(doc(firestore, "users", me.email), {
         uid: me.uid,
@@ -26,5 +28,46 @@
 {#if me.signedIn}
   <slot />
 {:else}
-  <Signin {auth} {googleAuthProvider} on:user_changed={user} />
+  <p class:loading>Loading...</p>
+  <span class:loading>
+    <Signin {auth} {googleAuthProvider} on:user_changed={user} />
+  </span>
 {/if}
+
+<style>
+  p {
+    display: none;
+  }
+  p.loading {
+    display: block;
+    animation: 1s ease 0s normal forwards 1 fadein;
+  }
+
+  @keyframes fadein {
+    0% {
+      opacity: 0;
+    }
+    66% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+
+  @-webkit-keyframes fadein {
+    0% {
+      opacity: 0;
+    }
+    66% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+
+  span.loading {
+    display: none;
+  }
+</style>
